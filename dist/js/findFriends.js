f155a8a62a997e2867012a53aea0adc24091f1cc
rmtar <picpoint@yandex.ru>
2020-01-10 22:06:03 +0300

    rewrite this functional to oop

--- dist/js/findFriends.js
+++ dist/js/findFriends.js
@@ -3,6 +3,44 @@ let findright = document.querySelector('.ff__findright').lastElementChild;      
 let leftblock = document.querySelector('.ff__pplleftblock');                                        // левый блок в котором осуществляется поиск
 let rightblock = document.querySelector('.ff__pplrightblock');                                      // правый блок в котором осуществляется поиск
 
+// findleft/findright  - findBlock                                                                  // левое и правое поле поиска именую для ооп - findBlock
+// leftblock/rightblock - lrBlock                                                                   // левый и правый блок в котором карточки друзей именую - lrBlock
+
+
+class FindFriends {                                                                                 // функционал в классе такой же как и в функциональной реализации
+  constructor(findBlock, lrBlock) {                                                                 // описание и методу работы можно прочитать из функциональной реализации
+    this.findBlock = findBlock;
+    this.lrBlock = lrBlock;
+  }
+
+  findFunction() {
+    this.findBlock.addEventListener('input', () => {
+      let findtext = this.findBlock.value.toLowerCase();
+
+      for(let i = 0; i < this.lrBlock.children.length; i++) {
+        let strName = this.lrBlock.children[i].firstElementChild.lastElementChild.innerText.toLowerCase();
+
+        if(findtext == strName.substr(0, findtext.length)) {
+          this.lrBlock.children[i].style = 'display: flex'
+        } else {
+          this.lrBlock.children[i].style = 'display: none'
+        }
+      }
+    });
+  }
+  
+}
+
+let lft = new FindFriends(findleft, leftblock);                                                     // вызываем класс для левого блока и левого поля
+lft.findFunction();
+
+let rght = new FindFriends(findright, rightblock);                                                  // вызываем класс для правого блока и правого поля
+rght.findFunction();
+
+
+
+
+/*  РЕАЛИЗАЦИЯ В ФУНКЦИОНАЛЬНОМ ПОДХОДЕ
 
 findleft.addEventListener('input', () => {                                                          // вешаем событие input на левое поле при вводе текста
   let findtext = findleft.value.toLowerCase();                                                      // переменной findtext присваиваем значение поля в нижнем регистре
@@ -34,3 +72,5 @@ findright.addEventListener('input', () => {                                     
   } 
 
 });
+
+*/
