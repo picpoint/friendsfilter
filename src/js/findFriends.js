104b77a9b5aa0bbc053ed9fc2f7cc93a5fd58ddd
picpoint <picpoint@yandex.ru>
2020-01-10 08:52:37 +0300

    made curve functional to find friends

--- src/js/findFriends.js
+++ src/js/findFriends.js
@@ -1,3 +1,20 @@
 let findleft = document.querySelector('.ff__findleft').lastElementChild;
+let leftblock = document.querySelector('.ff__pplleftblock');
 
 console.log(findleft);
+console.log(leftblock);
+
+findleft.addEventListener('input', () => {
+  console.log(findleft.value);  
+
+  for(let i = 0; i < leftblock.children.length; i++) {
+    console.log(leftblock.children[i]);
+    if(leftblock.children[i].firstElementChild.lastElementChild.innerText != findleft.value) {
+      leftblock.children[i].style = 'display: none';
+    } else {
+      leftblock.children[i].style = 'display: flex';
+    }
+  }
+  
+
+});
