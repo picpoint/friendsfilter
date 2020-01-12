1d84c34d1dc9af7e2b47d36bcb507bd32be8018b
rmtar <picpoint@yandex.ru>
2020-01-12 21:43:55 +0300

    write zeroing old cookie and add new cookie

--- src/js/saveLists.js
+++ src/js/saveLists.js
@@ -1,48 +1,49 @@
-let btn = document.querySelector('.ff__saveblock').firstElementChild;
-let lftblock = document.querySelector('.ff__pplleftblock');
-let rghtblock = document.querySelector('.ff__pplrightblock');
+let btn = document.querySelector('.ff__saveblock').firstElementChild;                                   // доступ к кнопке "сохранить"
+let lftblock = document.querySelector('.ff__pplleftblock');                                             // доступ к левому блоку
+let rghtblock = document.querySelector('.ff__pplrightblock');                                           // доступ к правому блоку
 
 
-window.addEventListener('load', () => {  
-  console.log('event load is completed');
+window.addEventListener('load', () => {                                                                 // событие загрузки страницы  
+  setTimeout(() => {                                                                                    // устанавливаем таймер на 3 сек
+    for(let x = 0; x < lftblock.children.length; x++) {                                                 // перебираем левый блок
+      let cookieValue = lftblock.children[x].getAttribute('id');                                        // в переменную cookieValue записываем значение id блока
+      document.cookie = `LeftBlocks${cookieValue}=-1; path=/; expires=`;                                // в куки левого блока, если они были раньше пишем -1      
 
-  setTimeout(() => {
-    for(let x = 0; x < lftblock.children.length; x++) {    
-      let cookieValue = lftblock.children[x].getAttribute('id');        
-      document.cookie = `LeftBlocks${cookieValue}=-1; path=/; expires=`;         
+      if(rghtblock.children.length == 0) {                                                              // если rghtblock пуст
+        document.cookie = `RightBlocks${cookieValue}=-1; path=/; expires=`;                             // в куки правого блока пишем -1
+      }
     }
   
-    for(let z = 0; z < rghtblock.children.length; z++) {
-      let cookieValue = rghtblock.children[z].getAttribute('id');        
-      document.cookie = `RightBlocks${cookieValue}=-1; path=/; expires=`;         
-    }
-  }, 3000);  
-  
+    for(let z = 0; z < rghtblock.children.length; z++) {                                                // перебираем правый блок
+      let cookieValue = rghtblock.children[z].getAttribute('id');                                       // получаем значение id элемента 
+      document.cookie = `RightBlocks${cookieValue}=-1; path=/; expires=`;                               // в куки правого блока пишем -1
+    }    
+
+  }, 3000);                                                                                             // делаем эту всю процедуру, чтобы старые куки
+});                                                                                                     // перезаписывались -1 и было понятно какие куки старые а какие нет
+
+
+
+
+btn.addEventListener('click', () => {                                                                   // событие по нажатию кнопки
+  for(let x = 0; x < lftblock.children.length; x++) {                                                   // перебираем левый блок
+    let cookieValue = lftblock.children[x].getAttribute('id');                                          // получаем значение id левого блока
+    document.cookie = `LeftBlocks${cookieValue}=${cookieValue}; path=/; expires=` + setTimeLiveCookie();// в куки пишем значение LeftBlocksid=id чтобы знать к какому блоку она относится
+  }                                                                                                     // и устанавливаем путь и время жизни из ф-ии setTimeLiveCookie();
+
+  for(let z = 0; z < rghtblock.children.length; z++) {                                                  // перебираем правый блок
+    let cookieValue = rghtblock.children[z].getAttribute('id');                                         // получаем значение id правого блока
+    document.cookie = `RightBlocks${cookieValue}=${cookieValue}; path=/; expires=` + setTimeLiveCookie();// в куки пишем значение RightBlocksid=id чтобы знать к какому блоку она относится
+  }                                                                                                     // и устанавливаем путь и время жизни из ф-ии setTimeLiveCookie();
 });
 
 
 
-btn.addEventListener('click', () => {  
-  console.log(lftblock);
-  console.log(rghtblock);
-  console.log('------------');
 
-  for(let x = 0; x < lftblock.children.length; x++) {    
-    let cookieValue = lftblock.children[x].getAttribute('id');        
-    document.cookie = `LeftBlocks${cookieValue}=${cookieValue}; path=/; expires=`;         
-  }
-
-  for(let z = 0; z < rghtblock.children.length; z++) {
-    let cookieValue = rghtblock.children[z].getAttribute('id');        
-    document.cookie = `RightBlocks${cookieValue}=${cookieValue}; path=/; expires=`;         
-  }
-  
-});
-
-
-// дописать ф-ию определяющую время жизни кук
-function endLiveTime() {
-  return -1;
+function setTimeLiveCookie() {                                                                          // ф-ия устанавливающая время жизни для кук
+  let tm = new Date();                                                                                  // определяем текущую дату и время
+  let timeLive = tm.getTime() * 60 * 60 * 24 * 7 * 1000;                                                // получаем время * 60 сек * 60 мин * 24 часа * 7 дней * 1000мл/сек
+  return timeLive;
 }
 
-// console.log(endLiveTime());
+
